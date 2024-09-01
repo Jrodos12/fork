e112dd70723d2eb12824a03cd1bce8f52a6deb69
Jrodos14 <trivero@fi.uba.ar>
2024-09-01 19:53:28 -0300

    avances

--- primes.c
+++ primes.c
@@ -12,7 +12,7 @@ void filter(int* fds_read, int n)
 	int primo = 0;
 	read(fds_read[0], &primo, sizeof(primo));
 	int id = fork();
-	if (primo == 0)
+	if (primo == 1)
 	{
 		close(fds_read[0]);
 		close(fds_write[1]);
@@ -33,52 +33,53 @@ void filter(int* fds_read, int n)
 	else
 	{
 		wait();
-		for (int i = 1; i <= 10; i++)
+		for (int i = 1; primo*i <= n; i++)
 		{
 			printf("%d \n",primo*i);
 		}
 		printf("\n");
 		
 	}
+}
 
-	/*
-	int num = 0;
-	int prime = 0;
-	read(fds_read[0], &prime, sizeof(prime));
-	printf("%d \n", prime);
-	if (prime == n){ exit(1);}
-	
+void filtro(int* fds_read, int n);
 
+void filtro(int* fds_read, int n)
+{
 	int fds_write[2];
 	int r = pipe(fds_write);
-	int  i = fork();
-	if (i == 0)
+	int primo = 0;
+	read(fds_read[0], &primo, sizeof(primo));
+	int id = fork();
+	if (id == 0)
 	{
-		prime ++;
-		write(fds_write[1], &(prime),sizeof(prime) );
-		filter(fds_write,n);
 		
+		filtro(fds_write, n);
+		
+		exit(1);
 	}
-	if (i > 0)
+	else
 	{
-		while (read(fds_read[0], &prime, sizeof(prime)) > 0)
+		read(fds_read[0], &primo,sizeof(primo));
+		printf("%d \n",primo);
+		while (read(fds_read[0], &primo,sizeof(primo)) != 0)
 		{
-			read(fds_read[0], &num, sizeof(n));
-			close(fds_read[1]);
-			if (n % prime != 0)
+			int num;
+			read(fds_read[0], &num,sizeof(num));
+			if (num % primo != 0)
 			{
-				write(fds_write[1], &prime, sizeof(prime));
-				close(fds_write[0]);
-				wait();
+				write(fds_write[1], &num, sizeof(num));
 			}
+			
 		}
 		
+
 	}
+
 	
-	
-	exit(1);
-	*/
+return;
 }
+
 main(int argc, char *argv[])
 {
 	int fds_write[2];
@@ -87,6 +88,6 @@ main(int argc, char *argv[])
 
 	write(fds_write[1], &primo, sizeof(primo));
 
-	filter(fds_write, atoi(argv[1]) );
+	filtro(fds_write, atoi(argv[1]) );
 	return 0;
 }
