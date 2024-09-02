380533b16fae8b6559f74be4e53ae12a32bd0a86
Jrodos12 <trivero@fi.uba.ar>
2024-09-02 13:28:16 -0300

    corrigo error de entrada

--- primes.c
+++ primes.c
@@ -3,91 +3,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void filter(int* fds_read, int n);
+void filter(int fds_read, int n);
 
-void filter(int* fds_read, int n)
+void filter(int fds_read, int n)
 {
-	int fds_write[2];
-	int r = pipe(fds_write);
-	int primo = 0;
-	read(fds_read[0], &primo, sizeof(primo));
-	int id = fork();
-	if (primo == 1)
+	int primo;
+	//read(fds_read[0], &primo, sizeof(primo));
+	//printf("voy a filtrar\n");
+	if (read(fds_read, &primo, sizeof(primo)) == 0)
 	{
-		close(fds_read[0]);
-		close(fds_write[1]);
-		close(fds_write[0]);
 		exit(1);
 	}
 	
-	if (id == 0)
-	{
-		
-		int next = primo - 1;
-		write(fds_write[1], &next, sizeof(next));
-		close(fds_read[1]);
-		filter(fds_write, n);
-		close(fds_write[0]);
+	printf("%d\n", primo);
 
-	}
-	else
-	{
-		wait();
-		for (int i = 1; primo*i <= n; i++)
-		{
-			printf("%d \n",primo*i);
-		}
-		printf("\n");
-		
-	}
-}
-
-void filtro(int* fds_read, int n);
-
-void filtro(int* fds_read, int n)
-{
 	int fds_write[2];
 	int r = pipe(fds_write);
-	int primo = 0;
-	read(fds_read[0], &primo, sizeof(primo));
+
 	int id = fork();
+
 	if (id == 0)
 	{
-		
-		filtro(fds_write, n);
-		
-		exit(1);
+		//child
+		//write(fds_write[1],&primo, sizeof(primo));
+		filter(fds_write[0], n);
+		close(fds_write[1]);
 	}
 	else
 	{
-		read(fds_read[0], &primo,sizeof(primo));
-		printf("%d \n",primo);
-		while (read(fds_read[0], &primo,sizeof(primo)) != 0)
+		int num;
+		while (read(fds_read, &primo, sizeof(primo)) > 0)
 		{
-			int num;
-			read(fds_read[0], &num,sizeof(num));
 			if (num % primo != 0)
 			{
 				write(fds_write[1], &num, sizeof(num));
+				close(fds_write[1]);
 			}
 			
 		}
 		
-
 	}
 
 	
-return;
+	return;
 }
 
 main(int argc, char *argv[])
 {
 	int fds_write[2];
 	int r = pipe(fds_write);
-	int primo = atoi(argv[1]) ;
+	int primo = 2 ;
 
 	write(fds_write[1], &primo, sizeof(primo));
-
-	filtro(fds_write, atoi(argv[1]) );
+	close(fds_write[1]);
+	filter(fds_write[0], atoi(argv[1]));
 	return 0;
 }
