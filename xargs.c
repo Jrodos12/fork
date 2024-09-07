fd29ad97c8fa34d0b962c96e3fe7b0e68a570dff
Jrodos12 <trivero@fi.uba.ar>
2024-09-07 02:38:41 -0300

    cambios en xargs

--- xargs.c
+++ xargs.c
@@ -10,7 +10,6 @@ void xargs(char* comand)
 	char *line = NULL;
 	size_t len = 0;
 	size_t n = 0;
-	//printf("recibi %s\n", comand);
 	int id  = fork();
 	
 	if (id == 0 )
@@ -34,30 +33,9 @@ void xargs(char* comand)
 int
 main(int argc, char *argv[])
 {
-	//printf("recibi %s\n", argv[1]);
     char *line = NULL;
     size_t len = 0;
 	size_t n = 0;
-	/*
-	int id  = fork();
-	
-	if (id == 0 )
-	{
-		//xargs();
-		//execvp(argv[1], argv+1);
-	}
-	else
-	{
-		wait();
-		char* strg[6] = {argv[1],0,0,0,0, NULL};
-		for (int i = 0; i < NARGS; i++)
-		{
-			n = getline(&strg[i+1], &len,stdin);
-			strg[i+1][n-1] = '\0'; 
-		}
-		execvp(argv[1], strg);
-	}
-	*/
 	xargs(argv[1]);
 	return 0;
 }
